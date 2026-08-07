10ec582c8d84c044cd58e0d3a94384c28aa576ab
Juloc <juli.hiresch@gmail.com>
2026-08-07 09:05:12 +0200

    Expose 1.0 repertoire in training page model

--- src/Nadiano.Web/Pages/Practice/Beta.cshtml.cs
+++ src/Nadiano.Web/Pages/Practice/Beta.cshtml.cs
@@ -2,10 +2,17 @@ using System.Globalization;
 
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using Nadiano.Core.Beta;
+
 namespace Nadiano.Web.Pages.Practice;
 
 public sealed class BetaModel : PageModel
 {
+    public IReadOnlyList<RepertoirePieceDescriptor> Repertoire { get; } = ReleaseContentCatalogue.Create().Repertoire;
+
     public string T(string german, string indonesian) =>
         CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "id" ? indonesian : german;
+
+    public string PieceTitle(RepertoirePieceDescriptor piece) =>
+        CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "id" ? piece.TitleId : piece.TitleDe;
 }
