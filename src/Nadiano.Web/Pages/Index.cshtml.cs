b1093c10d39234d046b8087281a3a4444331fe32
Juloc <juli.hiresch@gmail.com>
2026-08-07 21:16:44 +0200

    Drive Today from learner progress

--- src/Nadiano.Web/Pages/Index.cshtml.cs
+++ src/Nadiano.Web/Pages/Index.cshtml.cs
@@ -1,12 +1,34 @@
-using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Nadiano.Web.Features.Progress;
+using Nadiano.Web.Infrastructure.Profiles;
 
 namespace Nadiano.Web.Pages;
 
-public class IndexModel : PageModel
+public class IndexModel(
+    ProgressSummaryService progress,
+    CurrentProfileAccessor profiles) : PageModel
 {
-    public void OnGet()
-    {
+    public ProgressSummary Summary { get; private set; } = new(
+        0,
+        0,
+        [],
+        [],
+        [],
+        [],
+        [],
+        null,
+        null);
 
+    public async Task OnGetAsync(CancellationToken cancellationToken)
+    {
+        var profileId = await profiles.GetOrCreateProfileIdAsync(HttpContext, cancellationToken);
+        Summary = await progress.BuildAsync(
+            profileId,
+            CultureInfo.CurrentUICulture.TwoLetterISOLanguageName,
+            DateTimeOffset.UtcNow,
+            cancellationToken);
     }
 }
